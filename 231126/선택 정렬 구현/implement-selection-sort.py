c33b6adf645d219202a8472399985de7ab7e3a4e
opop8834 <92248074+opop8834@users.noreply.github.com>
2023-11-26 14:55:47 +0900

    Title: 선택 정렬 구현, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231126/선택 정렬 구현/implement-selection-sort.py
@@ -0,0 +1,23 @@
+# 선택정렬
+
+def selection_sort(arr):
+    for i in range(len(arr)-1):
+        min = i
+        for j in range(i , len(arr)-1):  # 최솟값 찾기
+            if arr[j] < arr[min]:
+                min = j
+        if arr[i]> arr[min]:
+            temp = arr[i]
+            arr[i] = arr[min]
+            arr[min] = temp
+        
+    return arr
+
+arr = []
+num = int(input())
+text = input().split()
+for s in text:
+    arr.append(int(s))
+arr = selection_sort(arr)
+for s in arr:
+    print(s, end=' ')
