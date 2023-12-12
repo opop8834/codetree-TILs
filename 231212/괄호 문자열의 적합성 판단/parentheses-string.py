9a2a2396b1d2faf0cd2b74a111d3666c5d35f7d8
opop8834 <92248074+opop8834@users.noreply.github.com>
2023-12-12 18:19:20 +0900

    Title: 괄호 문자열의 적합성 판단, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231212/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -0,0 +1,49 @@
+class Stack:
+    def __init__(self):
+        self.st = []
+    def push(self,value):
+        self.st.append(value)
+    def pop(self):
+        if len(self.st) <= 0:
+            raise Exception("Stack is empty")
+        return self.st.pop()
+    def size(self):
+        return len(self.st)
+    def empty(self):
+        if len(self.st) == 0:
+            return 1
+        else:
+            return 0
+    def top(self):
+        if len(self.st) <= 0:
+            raise Exception("Stack is empty")
+        return self.st[-1]
+
+# temp_stack = Stack()
+# num = int(input())
+# for i in range(num):
+#     text = input()
+#     if "push" in text:
+#         text = text.replace("push ", "")
+#         temp_stack.push(text)
+#     elif "size" in text:
+#         print(temp_stack.size())
+#     elif "empty" in text:
+#         print(temp_stack.empty())
+#     elif "pop" in text:
+#         print(temp_stack.pop())
+#     elif "top" in text:
+#         print(temp_stack.top())
+
+temp_stack = Stack()
+text = input()
+for i in range(len(text)):
+    temp = text[i]
+    if "(" == temp:
+        temp_stack.push(temp)
+    else:
+        temp_stack.pop()
+if temp_stack.empty() == 1:
+    print("Yes")
+else:
+    print("No")
