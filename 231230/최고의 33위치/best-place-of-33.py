c1dd279cbea2d3705f60b8287d0303f6e3e83f91
opop8834 <92248074+opop8834@users.noreply.github.com>
2023-12-30 12:40:13 +0900

    Title: 최고의 33위치, Time: 5082ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 231230/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,37 @@
+num = input()
+num = int(num)
+arr = [[0 for j in range(num)] for i in range(num)]
+
+for i in range(num):
+    text = input().split()
+    for j in range(num):
+        arr[j][i] = int(text[j])
+        
+print(arr)
+
+one = 0
+second = 0
+
+count = 0
+max_count = -99
+while(True):
+    if one+2 >= len(arr):
+        second += 1
+        one = 0
+    elif second+2 >= len(arr):
+        break
+    else:
+        count += arr[one][second]
+        count += arr[one+1][second]
+        count += arr[one+2][second]
+        count += arr[one][second+1]
+        count += arr[one+1][second+1]
+        count += arr[one+2][second+1]
+        count += arr[one][second+2]
+        count += arr[one+1][second+2]
+        count += arr[one+2][second+2]
+    if count > max_count:
+        max_count = count
+    count = 0
+    one += 1
+print(max_count)
