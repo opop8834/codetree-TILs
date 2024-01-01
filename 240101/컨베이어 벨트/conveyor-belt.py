6554effa6fa52b6e1b41a52536a2a5f80d3426b8
opop8834 <92248074+opop8834@users.noreply.github.com>
2024-01-01 19:36:05 +0900

    Title: 컨베이어 벨트, Time: 118ms, Memory: 28MB, Status: Passed - CodeTree

--- 240101/컨베이어 벨트/conveyor-belt.py
+++ 240101/컨베이어 벨트/conveyor-belt.py
@@ -16,7 +16,7 @@ for i in range(n):
 for i in range(t):
     temp_1 = arr[1][-1]
     temp_2 = arr[0][-1]
-    for j in range(len(arr), 0, -1):
+    for j in range(len(arr[0])-1, 0, -1):
         arr[0][j] = arr[0][j-1]
         arr[1][j] = arr[1][j-1]
     arr[0][0] = temp_1
