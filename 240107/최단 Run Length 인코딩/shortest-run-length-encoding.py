8312462c0f6a625988bea739ba8d969fc942cb5c
opop8834 <92248074+opop8834@users.noreply.github.com>
2024-01-07 14:40:43 +0900

    Title: 최단 Run Length 인코딩, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240107/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240107/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -1,10 +1,10 @@
 arr = input()
 arr = list(arr)
 min_count = 99
+tp = 0
 for s in arr:
     pre_number = arr[0]
     count = 2
-    tp = 0
     for i in range(len(arr)):
         # print("--------------------------------")
         # print(pre_number + " "+arr[i])
@@ -15,10 +15,10 @@ for s in arr:
         pre_number = arr[i]
     if count < min_count:
         min_count = count
-    if tp == 0:
-        min_count = 3
     temp_arr = arr[-1]
     for i in range(len(arr)-1,0,-1):
         arr[i] = arr[i-1]
     arr[0] = temp_arr
+if tp == 0:
+    min_count = 3
 print(min_count)
