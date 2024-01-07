ff7c0303f904875ad85cc54fea4c427353aba4c8
opop8834 <92248074+opop8834@users.noreply.github.com>
2024-01-07 14:38:26 +0900

    Title: 최단 Run Length 인코딩, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240107/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,24 @@
+arr = input()
+arr = list(arr)
+min_count = 99
+for s in arr:
+    pre_number = arr[0]
+    count = 2
+    tp = 0
+    for i in range(len(arr)):
+        # print("--------------------------------")
+        # print(pre_number + " "+arr[i])
+        # print("--------------------------------")
+        if pre_number != arr[i]:
+            count += 2
+            tp =1
+        pre_number = arr[i]
+    if count < min_count:
+        min_count = count
+    if tp == 0:
+        min_count = 3
+    temp_arr = arr[-1]
+    for i in range(len(arr)-1,0,-1):
+        arr[i] = arr[i-1]
+    arr[0] = temp_arr
+print(min_count)
