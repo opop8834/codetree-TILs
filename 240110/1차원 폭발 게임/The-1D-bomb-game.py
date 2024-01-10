9541b695e0f72fab995adcd7580f202198b82be7
opop8834 <92248074+opop8834@users.noreply.github.com>
2024-01-10 17:55:43 +0900

    Title: 1차원 폭발 게임, Time: 150ms, Memory: 28MB, Status: Passed - CodeTree

--- 240110/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240110/1차원 폭발 게임/The-1D-bomb-game.py
@@ -6,9 +6,34 @@ for i in range(N):
     text = int(input())
     arr.append(text)
 arr = arr[::-1]
-print(arr)
 
-for i in range(arr):
-    current_number = arr[i]
-    for j in range(M):
-        if
+for kk in range(N):
+    temp_arr = []
+    i = 0
+    while(i <= len(arr)-1):
+        tp = 0
+        count = 1
+        current_number = arr[i]
+        while(True):
+            if i+count <= len(arr)-1 and current_number == arr[i+count]:
+                count += 1
+            else:
+                if count >= M:
+                    tp = 1
+                break
+        if tp == 1:
+            i += count
+            count = 1
+            tp = 0
+        else:
+            temp_arr.append(current_number)
+            i+=1
+    arr.clear()
+    for ii in range(len(temp_arr)):
+        arr.append(temp_arr[ii])
+
+arr = arr[::-1]
+print(len(arr))
+if len(arr) != 0:
+    for i in range(len(arr)):
+        print(arr[i])
