76209e5d84fab6446fda7a3caec6db71069cc91b
opop8834 <92248074+opop8834@users.noreply.github.com>
2024-01-17 17:49:35 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240117/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -0,0 +1,35 @@
+n,r,c = input().split()
+n = int(n)
+r = int(r) - 1
+c = int(c) - 1
+arr = [[0 for i in range(n)] for i in range(n)]
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+for i in range(n):
+    text = input().split()
+    for j in range(n):
+        arr[i][j] = int(text[j])
+
+i = 0
+j = 0
+print(arr[r][c], end =' ')
+while(True):
+    if j >= n:
+        j = 0
+        i += 1
+    if i >= n:
+        break
+    if i == r and j == c:
+        for jj in range(4):
+            temp_number = arr[i+dx[jj]][j+dy[jj]]
+            current_number = arr[i][j]
+            if (temp_number > current_number):
+                print(temp_number, end = ' ')
+                r = i+dx[jj]
+                c = j+dy[jj]
+                i = 0
+                j = 0
+                break
+    j += 1
