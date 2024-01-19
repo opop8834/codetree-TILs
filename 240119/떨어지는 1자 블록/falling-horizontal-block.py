483ec13481392466d16a8843e2dc27f971f8a2ef
opop8834 <92248074+opop8834@users.noreply.github.com>
2024-01-19 15:44:59 +0900

    Title: 떨어지는 1자 블록, Time: 164ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240119/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,26 @@
+n,m,k = input().split()
+n = int(n)
+m = int(m)
+k = int(k) - 1
+
+arr = [[0 for i in range(n)] for j in range(n)]
+for i in range(n):
+    text = input().split()
+    for j in range(n):
+        arr[i][j] = int(text[j])
+
+temp_range = k + m - 1
+for i in range(n-1, -1, -1):
+    tp = 0
+    for j in range(k,temp_range+1):
+        current_number = arr[i][j]
+        if current_number == 1:
+            tp = 1
+    if tp == 0:  # 0 일때 input넣기
+        for jj in range(k,temp_range+1):
+            arr[i][jj] = 1
+        break
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j], end = " ")
+    print()
