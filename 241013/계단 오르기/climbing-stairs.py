edff389142c578dd7da1bc168722abd7edc9773f
opop8834 <92248074+opop8834@users.noreply.github.com>
2024-10-13 19:39:09 +0900

    Title: 계단 오르기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241013/계단 오르기/climbing-stairs.py
@@ -0,0 +1,26 @@
+n = int(input())
+dp = [-1] * (n+1)
+# dp[4] = 1  dp[2] + dp[2]
+# dp[5] = 2  dp[3] + dp[2]
+# dp[6] = 2  dp[3] + dp[3]  or dp[2] dp[2] dp[2]
+# dp[7] = 3  dp[3] + dp[3] + dp[2]
+
+# 2나 3으로 나눌때 0이 되면 1
+if n <=1:
+    print(0)
+elif n <=3:
+    print(1)
+else:
+    dp[0] = 0
+    dp[1] = 0
+    dp[2] = 1
+    dp[3] = 1
+    for i in range(4,n+1):
+        # count = 0
+        # if i % 2 == 0:
+        #     count += 1
+        # if i % 3 == 0:
+        #     count += 1
+        
+        dp[i] = dp[i-2] + dp[i-3]
+    print(dp[-1])
