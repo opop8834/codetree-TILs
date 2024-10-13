93a6de7f5da7b0a8547570ac371d315e3731f625
opop8834 <92248074+opop8834@users.noreply.github.com>
2024-10-13 19:13:01 +0900

    Title: 피보나치 수, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 241013/피보나치 수/fibonacci-number.py
+++ 241013/피보나치 수/fibonacci-number.py
@@ -1,10 +1,12 @@
 n = int(input())
 dp = [-1] * n
-dp[0] = 1
-dp[1] = 1
+
+
 if n <= 1:
     print(1)
 else:
+    dp[0] = 1
+    dp[1] = 1
     for i in range(2,n):
         dp[i] = dp[i-1] + dp[i-2]
     print(dp[-1])
