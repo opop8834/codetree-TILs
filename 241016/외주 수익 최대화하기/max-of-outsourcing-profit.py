728a538beae0b088a67c993b5f5f4f01bf0753d3
opop8834 <92248074+opop8834@users.noreply.github.com>
2024-10-16 01:28:57 +0900

    Title: 외주 수익 최대화하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241016/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ 241016/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -10,20 +10,17 @@ for i in range(n):
 comb = []
 for i in range(1,n+1):
     comb.append(list(itertools.combinations(arr,i)))
-    
 
 max_p = -999
-sum_t = 0
-sum_p = 0
-for s in comb:
-    for ss in s:
-        for sss in ss:
+for s in comb:  # 1개의 조합, 2개의 조합 등등
+    for ss in s: # ~~개의 조합 원소들을 출력
+        sum_t = 0
+        sum_p = 0
+        for sss in ss:  # 조합 안의 원소 추출
             sum_t += sss[0]
             sum_p += sss[1]
         if sum_t <= n and sum_p > max_p:
             max_p = sum_p
-            sum_t = 0
-            sum_p = 0
             
 
 if max_p == -999:
