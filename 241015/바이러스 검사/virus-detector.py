6017a1a7a9ea9fda81fd5de777f6d2b7af4c7806
opop8834 <92248074+opop8834@users.noreply.github.com>
2024-10-15 20:51:34 +0900

    Title: 바이러스 검사, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241015/바이러스 검사/virus-detector.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+count = list(map(int,input().split(" ")))
+
+# 팀장 최대 , 팀원 최대
+owner, team = map(int,input().split(" "))
+sum_ = 0
+for s in count:
+    if s > owner:
+        temp = s - owner
+        temp_2 = int(temp / team)
+        if temp % team> 0:
+            temp_2 += 1
+            
+        # 팀장 까지 합
+        temp_2 += 1    
+        sum_ += temp_2
+    else:
+        sum_ += 1
+print(sum_)
