8da17cd92876fd251856368122f5093b921211ca
opop8834 <92248074+opop8834@users.noreply.github.com>
2024-10-19 03:28:15 +0900

    Title: 바이러스 백신, Time: 168ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241019/바이러스 백신/vaccine-for-virus.py
+++ 241019/바이러스 백신/vaccine-for-virus.py
@@ -29,7 +29,7 @@ def bfs(s):
         x,y = q.popleft()
         for i in range(4):
             new_x, new_y = x + dx[i], y + dy[i]
-            if new_x >=0 and new_x < n and new_y >=0 and new_y <n and graph[new_x][new_y] ==0 and not visited[new_x][new_y]:
+            if new_x >=0 and new_x < n and new_y >=0 and new_y <n and graph[new_x][new_y] != 1 and not visited[new_x][new_y]:
                 visited[new_x][new_y] = True
                 q.append((new_x, new_y))
         temp_len += 1
